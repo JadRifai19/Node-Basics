40b2ea813b8b49ed7586a2a6a19e021e1c18dbb1
JadRifai19 <jad.rami.rifaii19@gmail.com>
2022-12-23 10:53:18 +0200

    exit command

--- tasks.js
+++ tasks.js
@@ -34,7 +34,7 @@ function startApp(name){
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text === 'quit\n') {
+  if (text === 'quit\n' || text === 'exit\n' )  {
     quit();
   }
   else if(text === 'hello\n'){
@@ -76,6 +76,7 @@ function hello(){
 function quit(){
   console.log('Quitting now, goodbye!')
   process.exit();
+
 }
 
 // The following line starts the application
